d2b89717cd1cf94d894685ac3c48d558e6365952
Harish Mandhadi <harishh486@gmail.com>
2017-11-30 20:41:53 -0500

    Initial commit--D.I using Qualifiers and Primary Beans

---
+++ src/main/java/com/springframework/DiDemoApplication.java
@@ -0,0 +1,24 @@
+package com.springframework;
+
+import com.springframework.controllers.ConstructorInjectedController;
+import com.springframework.controllers.GetterInjectedController;
+import com.springframework.controllers.MyController;
+import com.springframework.controllers.PropertyInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean(MyController.class);
+
+		System.out.println(controller.hello());
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+	}
+}
---
+++ src/main/java/com/springframework/controllers/PropertyInjectedController.java
@@ -0,0 +1,23 @@
+package com.springframework.controllers;
+
+import com.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+@Primary
+public class PropertyInjectedController {
+
+    @Autowired
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
+
+    public String sayHello(){
+        return greetingServiceImpl.sayGreeting();
+    }
+
+}
